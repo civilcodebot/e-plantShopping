022180b1250413cc7f336ecc7130914df5472fbc
CivilCodeBot <lk9048+git@gmail.com>
2025-07-27 06:38:50 -0400

    minor update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -258,13 +258,16 @@ function ProductList({ onHomeClick }) {
         setShowCart(false);
     };
     const handleAddToCart = (product) => {
-        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
-      
-        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
-          ...prevState, // Spread the previous state to retain existing entries
-          [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+        dispatch(addItem({
+            name: product.name,
+            image: product.image,
+            cost: product.cost
         }));
-      };
+        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
+            ...prevState, // Spread the previous state to retain existing entries
+            [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+        }));
+    };
     return (
         <div>
             <div className="navbar" style={styleObj}>
